ef98c74ef1560b9d1efcb7ea57a6e2cc105bcb34
Balint Rikker <balint.rikker@prezi.com>
2018-12-01 18:11:46 +0100

    Finished day 01

--- day-01-chronal-calibration/main.cpp
+++ day-01-chronal-calibration/main.cpp
@@ -1,24 +1,54 @@
 #include <iostream>
+#include <set>
 #include <assert.h>
+#include <optional>
+#include <vector>
 
-// Read from stdin
-int main() {
-	long int total_frequency = 0;
-	bool is_eof;
-	while (std::iostream::traits_type::not_eof(std::cin.peek())) {
-		char sign;
+std::vector<long int> parseInput() {
+	std::vector<long int> v;
+	char sign;
+	while (std::cin >> sign) {
 		long int frequency_change;
-		std::cin >> sign;
 		std::cin >> frequency_change;
 		if (sign == '-') {
 			frequency_change *= -1;
 		} else {
 			assert(sign == '+');
 		}
+		v.push_back(frequency_change);
+	}
+	return v;
+}
+// Read from stdin
+int main() {
+	std::vector<long int> input = parseInput();
+
+	long int total_frequency = 0;
+	for (auto frequency_change : input) {
 		total_frequency += frequency_change;
-		std::cin.get();
 	}
 
-	std::cout << total_frequency << std::endl;
+	auto curr_total = 0;
+	std::set<long int> reached_freqs = { curr_total };
+	std::optional<long int> first_reacked_twice = {};
+	auto index = 0;
+
+	while (!first_reacked_twice.has_value()) {
+		auto curr_val = input[index];
+
+		curr_total += curr_val;
+		if (reached_freqs.find(curr_total) == reached_freqs.end()) {
+			reached_freqs.insert(curr_total);
+		} else {
+			first_reacked_twice = { curr_total };
+		}
+
+		index = (index + 1) % input.size();
+	}
+
+
+	std::cout << "total_frequency: " << total_frequency << std::endl;
+
+	std::cout << "first_reached_twice: " << first_reacked_twice.value() << std::endl;
 	return 0;
 }
